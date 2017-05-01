4d29b8d34fe9739d4c260f9b5ec4ab7be42d2aee
AbdoAllah Akram <AbdoAllah Akram>
2017-05-01 11:20:44 +0200

    add loading page

--- js/custom.js
+++ js/custom.js
@@ -8,19 +8,31 @@ $(document).ready(function () {
 
     // Change Style Color From Check Gear
     var liColor = $(".color-option ul li");
-        
-        liColor
+
+    liColor
         .eq(0).css("backgroundColor", "#65af00").end()
         .eq(1).css("backgroundColor", "#C62828").end()
         .eq(2).css("backgroundColor", "#FF9800").end()
         .eq(3).css("backgroundColor", "#9C27B0")
-    
-    liColor.click(function() {
-        console.log($(this).attr("data-value")) 
+
+    liColor.click(function () {
+        console.log($(this).attr("data-value"))
         $("link[href*='theme']").attr("href", "css/" + $(this).attr("data-value") + ".css")
         $("img[src*='icon_cloud']").attr("src", "images/icon_cloud_" + $(this).attr("data-value") + ".png")
-        
+
     });
-    
+
+    // Loading Page
+    $(window).ready(function () {
+
+
+        $(".loading .spinner").fadeOut(1000,
+            function () {
+                $("body").css("overflow", "auto");
+                $(".loading").fadeOut(1000);
+            });
+
+    });
+
 
 });
