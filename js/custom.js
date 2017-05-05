62eb27af85a378829f63181116e23ac8bc775cc5
AbdoAllah Akram <AbdoAllah Akram>
2017-05-05 12:56:27 +0200

    clean code css

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
+/*global $, jQuery, alert*/
 /*jshint -W117*/
 $(document).ready(function () {
-
+    'use strict';
     // Show Color Option Div When Click On   Chick Gear
     $(".check-gear").click(function () {
         $(".color-option").fadeToggle();
@@ -13,33 +14,50 @@ $(document).ready(function () {
         .eq(0).css("backgroundColor", "#65af00").end()
         .eq(1).css("backgroundColor", "#C62828").end()
         .eq(2).css("backgroundColor", "#FF9800").end()
-        .eq(3).css("backgroundColor", "#9C27B0")
+        .eq(3).css("backgroundColor", "#9C27B0");
 
     liColor.click(function () {
-        console.log($(this).attr("data-value"))
-        $("link[href*='theme']").attr("href", "css/" + $(this).attr("data-value") + ".css")
-        $("img[src*='icon_cloud']").attr("src", "images/icon_cloud_" + $(this).attr("data-value") + ".png")
+        $("link[href*='theme']").attr("href", "css/" + $(this).attr("data-value") + ".css");
+        $("img[src*='icon_cloud']").attr("src", "images/icon_cloud_" + $(this).attr("data-value") + ".png");
 
     });
 
-    
-    setTimeout(function(){
-         // Loading Page
-    $(window).ready(function () {
 
+    setTimeout(function () {
+        // Loading Page
+        $(window).ready(function () {
 
-        $(".loading .spinner").fadeOut(1000,
-            function () {
-                $(".loading").fadeOut(1000,
-                    function () {
-                        $(".loading").remove();
-                    }
-                );
-            });
+            $(".loading .spinner").fadeOut(1000,
+                function () {
+                    $(".loading").fadeOut(1000,
+                        function () {
+                            $(".loading").remove();
+                        } );
+                });
 
-    });
+        });
     }, 2000);
- 
 
+
+    // -------- Scroll Up Arrow -------- \\
+
+    // Cashing Scroll Arrow
+    var scrollArrow = $("#scroll-arrow");
+    
+    // Show Scroll Arrow in offset 900
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() >= 900)
+            scrollArrow.fadeIn();
+        else
+            scrollArrow.fadeOut();
+    });
+
+    // Scroll Up on click
+    scrollArrow.click(function () {
+        $("html,body").animate({
+            scrollTop: 0
+        }, 600);
+    });
 
 });
